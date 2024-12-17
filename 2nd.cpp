c2061c66ff3c77716efd29342e200fdc682c0323
Turja Chakraborty <152412436+Turjacb1@users.noreply.github.com>
2024-12-18 00:25:03 +0600

    add

---
+++ 2nd.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+#include<vector>
+using namespace std;
+
+int main()
+{
+    vector<string>cars={"BMW","TESLA","RANGE ROVER","PARDO"};
+
+    
+    
+        cout<<cars[0]<<"\n";
+        cout<<cars[1]<<"\n";
+
+    
+    return 0;
+}
