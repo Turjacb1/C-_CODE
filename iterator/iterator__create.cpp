fcac953ea66285543cdf525dac8cddf66b71621c
Turja Chakraborty <152412436+Turjacb1@users.noreply.github.com>
2024-12-18 18:41:47 +0600

    ireator,map

---
+++ iterator/iterator__create.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+int main() {
+  vector<string> cars = {"Volvo", "BMW", "Ford", "Mazda"};
+  vector<string>::iterator it;
+
+ 
+  for (it = cars.begin(); it != cars.end(); ++it) {
+    cout << *it<< "\n";
+  }
+
+  return 0;
+}
